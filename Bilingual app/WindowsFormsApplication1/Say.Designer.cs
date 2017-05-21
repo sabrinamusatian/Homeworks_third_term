88e38ca699cccc4b061eaf1571f7cd2ed4208c1d
Your Name <sabrina.musatian@yandex.ru>
2017-05-21 18:33:53 +0300

    bilingual app

---
+++ Bilingual app/WindowsFormsApplication1/Say.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Say
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Say));
+            this.hi = new System.Windows.Forms.Button();
+            this.howAreYou = new System.Windows.Forms.Button();
+            this.myName = new System.Windows.Forms.Button();
+            this.good = new System.Windows.Forms.Button();
+            this.Ilive = new System.Windows.Forms.Button();
+            this.Goodbye = new System.Windows.Forms.Button();
+            this.bye = new System.Windows.Forms.Button();
+            this.soso = new System.Windows.Forms.Button();
+            this.thankYou = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // hi
+            // 
+            resources.ApplyResources(this.hi, "hi");
+            this.hi.Name = "hi";
+            this.hi.UseVisualStyleBackColor = true;
+            this.hi.Click += new System.EventHandler(this.hi_Click);
+            // 
+            // howAreYou
+            // 
+            resources.ApplyResources(this.howAreYou, "howAreYou");
+            this.howAreYou.Name = "howAreYou";
+            this.howAreYou.UseVisualStyleBackColor = true;
+            this.howAreYou.Click += new System.EventHandler(this.howAreYou_Click);
+            // 
+            // myName
+            // 
+            resources.ApplyResources(this.myName, "myName");
+            this.myName.Name = "myName";
+            this.myName.UseVisualStyleBackColor = true;
+            this.myName.Click += new System.EventHandler(this.myName_Click);
+            // 
+            // good
+            // 
+            resources.ApplyResources(this.good, "good");
+            this.good.Name = "good";
+            this.good.UseVisualStyleBackColor = true;
+            this.good.Click += new System.EventHandler(this.good_Click);
+            // 
+            // Ilive
+            // 
+            resources.ApplyResources(this.Ilive, "Ilive");
+            this.Ilive.Name = "Ilive";
+            this.Ilive.UseVisualStyleBackColor = true;
+            this.Ilive.Click += new System.EventHandler(this.Ilive_Click);
+            // 
+            // Goodbye
+            // 
+            resources.ApplyResources(this.Goodbye, "Goodbye");
+            this.Goodbye.Name = "Goodbye";
+            this.Goodbye.UseVisualStyleBackColor = true;
+            this.Goodbye.Click += new System.EventHandler(this.Goodbye_Click);
+            // 
+            // bye
+            // 
+            resources.ApplyResources(this.bye, "bye");
+            this.bye.Name = "bye";
+            this.bye.UseVisualStyleBackColor = true;
+            this.bye.Click += new System.EventHandler(this.bye_Click);
+            // 
+            // soso
+            // 
+            resources.ApplyResources(this.soso, "soso");
+            this.soso.Name = "soso";
+            this.soso.UseVisualStyleBackColor = true;
+            this.soso.Click += new System.EventHandler(this.soso_Click);
+            // 
+            // thankYou
+            // 
+            resources.ApplyResources(this.thankYou, "thankYou");
+            this.thankYou.Name = "thankYou";
+            this.thankYou.UseVisualStyleBackColor = true;
+            this.thankYou.Click += new System.EventHandler(this.thankYou_Click);
+            // 
+            // comboBox1
+            // 
+            resources.ApplyResources(this.comboBox1, "comboBox1");
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // Say
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.thankYou);
+            this.Controls.Add(this.soso);
+            this.Controls.Add(this.bye);
+            this.Controls.Add(this.Goodbye);
+            this.Controls.Add(this.Ilive);
+            this.Controls.Add(this.good);
+            this.Controls.Add(this.myName);
+            this.Controls.Add(this.howAreYou);
+            this.Controls.Add(this.hi);
+            this.Name = "Say";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button hi;
+        private System.Windows.Forms.Button howAreYou;
+        private System.Windows.Forms.Button myName;
+        private System.Windows.Forms.Button good;
+        private System.Windows.Forms.Button Ilive;
+        private System.Windows.Forms.Button Goodbye;
+        private System.Windows.Forms.Button bye;
+        private System.Windows.Forms.Button soso;
+        private System.Windows.Forms.Button thankYou;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ Bilingual app/WindowsFormsApplication1/Say.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Globalization;
+using System.Threading;
+using System.Resources;
+using System.Reflection;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Say : Form
+    {
+        private ResourceManager m_resourceManger = null;
+        public Say()
+        {
+            m_resourceManger = new ResourceManager("Say.Say",
+                Assembly.GetExecutingAssembly());
+            InitializeComponent();
+            comboBox1.Items.Add("English");
+            comboBox1.Items.Add("Deutch");
+        }
+
+        private void hi_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Привет!");
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex == 1)
+            {
+                Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("de-DE");
+                Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("de-DE");
+                Controls.Clear();
+                InitializeComponent();
+                comboBox1.Items.Add("English");
+                comboBox1.Items.Add("Deutch");
+            }
+            else
+            {
+                Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("en-US");
+                Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en-US");
+                Controls.Clear();
+                InitializeComponent();
+                comboBox1.Items.Add("English");
+                comboBox1.Items.Add("Deutch");
+            }
+        }
+
+        private void howAreYou_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Как дела?");
+        }
+
+        private void myName_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Меня зовут...");
+        }
+
+        private void Goodbye_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("До свидания!");
+        }
+
+        private void good_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Хорошо");
+        }
+
+        private void Ilive_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Я живу в...");
+        }
+
+        private void bye_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Пока");
+        }
+
+        private void soso_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Так себе");
+        }
+
+        private void thankYou_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Спасибо!");
+        }
+    }
+}
